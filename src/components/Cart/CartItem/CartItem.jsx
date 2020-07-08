5298a13d5cd073dd0c0050d32a9d0ddbaf01c4bb
Alfredo Scunderlick Neto <asn94@outlook.com>
2020-07-08 12:08:49 -0300

    chore: add product to cart

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -9,16 +9,23 @@ const CartItem = ({ data, increment, decrement, remove }) => {
     <div className="cart__content">
       <div className="img">
         <img src={product.image} alt={product.name} />
-        <span onClick={() => remove(product.id)} className="cart__remove">
+        <span
+          onClick={() => remove(product.id, product.selectedSize.sku)}
+          className="cart__remove"
+        >
           Remover
         </span>
       </div>
       <div className="cart__detail">
         <h4>{product.name}</h4>
-        <p>Tam.: G</p>
-        <button onClick={() => decrement(product.id)}>-</button>
+        <p>Tam.: {product.selectedSize.size}</p>
+        <button onClick={() => decrement(product.id, product.selectedSize.sku)}>
+          -
+        </button>
         <span>{product.quantity}</span>
-        <button onClick={() => increment(product.id)}>+</button>
+        <button onClick={() => increment(product.id, product.selectedSize.sku)}>
+          +
+        </button>
       </div>
       <div className="cart__price">
         <h4>{product.actual_price}</h4>
