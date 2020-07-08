3a7bb8d04eb7e8e255b6fc1e1351612a73de508d
chicofronza <chicofronza@gmail.com>
2020-07-08 02:55:09 -0300

    Validate content in productSearch

--- src/routes/ProductRoute/ProductRoute.jsx
+++ src/routes/ProductRoute/ProductRoute.jsx
@@ -1,40 +1,63 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect } from "react";
 import "./SingleProduct.scss";
 import "./ProductRoute.scss";
 import Magnifier from "react-magnifier";
 import ProductContent from "../../components/ProductContent";
 
-import { useCart } from '../../hooks/cart';
-import reducer, { KEYS, INITIAL_STATE, clear, updateValue } from '../HomeRoute/duck';
-
+import { useCart } from "../../hooks/cart";
 
 const ProductRoute = (props) => {
-  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
   const { addToCart } = useCart();
   const { match } = props;
   const { id } = match.params;
-  const [productSearch, setProductSearch] = useState({});
-  const API_URL = 'https://undefined.netlify.app/api/catalog';
-  
+  const [productSearch, setProductSearch] = useState(null);
+  const API_URL = "https://undefined.netlify.app/api/catalog";
+
   useEffect(() => {
-    function fetchData() {
-      const response = fetch(API_URL);
-      const result = response.json();
-      
-      const withIdProducts = [];
-      result.map( (product) => {
-        const normalizeName = product.name.toLowerCase().replace(/\s/g, "-");
-        const idNormalized = `${normalizeName}-${product.code_color}`;
-        const productWithID = {...product, id:idNormalized};
-        withIdProducts.push(productWithID);
-      })
-      setProductSearch(withIdProducts.find(product => product.id === id));
-    }
-    fetchData();
-  });
+    // function fetchData() {
+    //   const response = fetch(API_URL);
+    //   const result = response.json();
 
+    //   const withIdProducts = [];
+    //   result.map((product) => {
+    //     const normalizeName = product.name.toLowerCase().replace(/\s/g, "-");
+    //     const idNormalized = `${normalizeName}-${product.code_color}`;
+    //     const productWithID = { ...product, id: idNormalized };
+    //     withIdProducts.push(productWithID);
+    //   });
+    //   setProductSearch(withIdProducts.find((product) => product.id === id));
+    //   console.log("=====Teste=======");
+    //   console.log(withIdProducts);
+    // }
+    // fetchData();
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          const withIdProducts = [];
+          result.map((product) => {
+            const normalizeName = product.name
+              .toLowerCase()
+              .replace(/\s/g, "-");
+            const idNormalized = `${normalizeName}-${product.code_color}`;
+            const productWithID = { ...product, id: idNormalized };
+            withIdProducts.push(productWithID);
+          });
+          let testeFilter = withIdProducts.find((product) => product.id === id);
+          console.log("=====Teste filter=======");
+          console.log(testeFilter);
+          setProductSearch(withIdProducts.find((product) => product.id === id));
+          console.log("=====Teste=======");
+          console.log(withIdProducts);
+        },
+        (error) => {}
+      );
+  }, []);
 
-  const [selectedSize, setSelectedSize] = useState('');
+  console.log("=====Teste 222=======");
+  console.log(productSearch);
+
+  const [selectedSize, setSelectedSize] = useState("");
 
   function handleClick(e, sku) {
     setSelectedSize(sku);
@@ -42,24 +65,26 @@ const ProductRoute = (props) => {
 
   return (
     <div>
-      <div className="single-product">
-        <figure className="product__image">
-          {/* <img src={props.image} alt="Product" /> */}
-          <Magnifier
-            zoomFactor={1.3}
-            mgWidth={180}
-            mgHeight={180}
-            mgShape="square"
-            src={productSearch.image}
-            alt={`Produto ${productSearch.image.name}`}
+      {productSearch != null && (
+        <div className="single-product">
+          <figure className="product__image">
+            {/* <img src={props.image} alt="Product" /> */}
+            <Magnifier
+              zoomFactor={1.3}
+              mgWidth={180}
+              mgHeight={180}
+              mgShape="square"
+              src={productSearch.image}
+              alt={`Produto ${productSearch.image.name}`}
+            />
+          </figure>
+          <ProductContent
+            product={productSearch}
+            selectedSize={selectedSize}
+            handleClick={handleClick}
           />
-        </figure>
-        <ProductContent
-          product={productSearch}
-          selectedSize={selectedSize}
-          handleClick={handleClick}
-        />
-      </div>
+        </div>
+      )}
     </div>
   );
 };
